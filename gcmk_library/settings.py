ebffc60c4f65fd64b3677bd7c6b9d81d5899beac
Neil Smith <neil.git@njae.me.uk>
2019-01-08 11:42:50 +0000

    Set allowed_hosts

--- gcmk_library/settings.py
+++ gcmk_library/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cwfsx3pjaenkykmmfv=7gz052vojjr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fathomless-scrubland-17825.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
